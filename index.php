3d824347e31407db291a08becb9565fc036befb2
unknown <unknown>
2021-12-12 11:21:20 +0100

    test

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+  <title>Traitement des donn&eacute;es du formulaire</title>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="style.css" />
+  
+  <!-- include jQuery library --> 
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script> 
+  
+  <script src="index.js"></script>
+
+</head>
+<body>
+  <header>
+
+
+  </header>
+
+  <section>
+  <!php>
+  
+  
+  
+  
+  </php>
+  
+  
+  
+
+  </section>
+  
+  <footer>
+  
+  
+  </footer>
+  
+</body>
+</html>
+
