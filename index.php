fbdcbaccbd2ffda68876ecdfe806776bc38e5b7a
unknown <unknown>
2021-12-12 13:18:29 +0100

    test

--- index.php
+++ index.php
@@ -23,17 +23,25 @@
   
     if (!empty($_GET)) {
   
-    echo "Traitement du formulaire";
-    
-    echo $_GET['form1Nom'];
-    echo $_GET['form1Prenom'];
-    echo $_GET['form1Mail'];
-    echo $_GET['form1Tel'];
-    echo $_GET['form1Pass'];
-    echo $_GET['form1Role'];
+      echo "Traitement du formulaire method GET";
+      
+      echo $_GET['form1Nom'];
+      echo $_GET['form1Prenom'];
+      echo $_GET['form1Mail'];
+      echo $_GET['form1Tel'];
+      echo $_GET['form1Pass'];
+      echo $_GET['form1Role'];
     
     } else if (!empty($_POST)) {
+        echo "Traitement du formulaire method GET";
     
+      echo $_POST['form1Nom'];
+      echo $_POST['form1Prenom'];
+      echo $_POST['form1Mail'];
+      echo $_POST['form1Tel'];
+      echo $_POST['form1Pass'];
+      echo $_POST['form1Role'];
+
     } else {
       echo("<p>Le Formulaire n'a pas &eacute;t&eacute; rempli");
     }
