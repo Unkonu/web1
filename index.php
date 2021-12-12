e8c071ef770b22ace52f397d380897f0ca7215a8
unknown <unknown>
2021-12-12 13:43:07 +0100

    test

--- index.php
+++ index.php
@@ -22,29 +22,41 @@
   
     if (!empty($_GET)) {
   
-      echo "Traitement du formulaire method GET";
+      echo "Traitement du formulaire method GET <br/>";
       
-      echo $_GET['form1Nom'];
-      echo "<br/>";
-      echo $_GET['form1Prenom'];
-      echo $_GET['form1Mail'];
-      echo $_GET['form1Tel'];
-      echo $_GET['form1Pass'];
-      echo $_GET['form1Role'];
+      define('nom',$_GET['form1Nom']);
+      define('prenom',$_GET['form1Prenom']);
+      define('mail',$_GET['form1Mail']);
+      define('tel',$_GET['form1Tel']);
+      define('password',$_GET['form1Pass']);
+      define('role',$_GET['form1Role']); 
     
     } else if (!empty($_POST)) {
         echo "Traitement du formulaire method GET";
     
-      echo $_POST['form1Nom'];
-      echo $_POST['form1Prenom'];
-      echo $_POST['form1Mail'];
-      echo $_POST['form1Tel'];
-      echo $_POST['form1Pass'];
-      echo $_POST['form1Role'];
+      define('nom',$_POST['form1Nom']);
+      define('prenom',$_POST['form1Prenom']);
+      define('mail',$_POST['form1Mail']);
+      define('tel',$_POST['form1Tel']);
+      define('password',$_POST['form1Pass']);
+      define('role',$_POST['form1Role']); 
 
     } else {
       echo("<p>Le Formulaire n'a pas &eacute;t&eacute; rempli");
     }
+    
+    echo $nom;
+    echo "<br/>";
+    echo $prenom;
+    echo "<br/>";
+    echo $mail;
+    echo "<br/>";
+    echo $tel;
+    echo "<br/>";
+    echo $password;
+    echo "<br/>";
+    echo $role;
+    echo "<br/>";
   
   ?>
   
