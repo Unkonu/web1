63eccd586bd1f3a1b921a899b96ec3ad4768f977
unknown <unknown>
2021-12-14 16:21:39 +0100

    test

--- courses.php
+++ courses.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <title>Titre de la page</title>
+  <title>Liste des courses</title>
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="style.css" />
   
@@ -13,43 +13,18 @@
 </head>
 <body>
   <header>
-    <a href="liste.php">Afficher liste des utilisateurs</a>
   </header>
 
   <section>
   
-  <div class="dform1">
-  <form id="fform1" enctype="multipart/form-data" method="post" action="./index.php">
-  <!--form id="fform1" method="get" action="./index.php"-->
-    <fieldset>
+  <div class="courses">
+  <form id="courses" enctype="multipart/form-data" method="post" action="courses.php">
+   <fieldset>
     <legend>Formulaire</legend>
-    <p class="champsForm1">
+      <label for="newCourse"></label>
+      <input id="newCourse" name="newCourse" type="text" value="Nom" />
       <label for="form1Nom">      Nom      </label>
-      <input id="form1Nom" name="form1Nom" type="text" value="Nom" />
-    </p>
-    <p class="champsForm1">
-      <label for="form1Prenom">      Pr&eacute;nom      </label>
-      <input id="form1Prenom" name="form1Prenom" type="text" value="Pr&eacute;nom" />
-    </p>
-    <p class="champsForm1">
-      <label for="form1Mail">      Mail      </label>
-      <input id="form1Mail" name="form1Mail" type="text" value="Mail" />
-    </p>
-    <p class="champsForm1">
-      <label for="form1Tel">      T&eacute;l&eacute;phone      </label>
-      <input id="form1Tel" name="form1Tel" type="text" value="Telephone" />
-    </p>
-    <p class="champsForm1">
-      <label for="form1Pass">      Mot de passe      </label>
-      <input id="form1Pass" name="form1Pass" type="text" value="Mot de passe" />
-    </p>
-    <p class="champsForm1">
-      <label for="form1Role">      Role      </label>
-      <input id="form1Role" name="form1Role" type="text" value="Role" />
-    </p>
-    <p class="boutonForm1">
-      <input id="form1Valide" type="submit" value="Valider le formulaire" />
-    </p>
+      <input id="form1Nom" name="form1Nom" type="button" value="Nom" />
     </fieldset>
   </form>
   
