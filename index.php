3f337613add8428e646bc959d7f9cd50316a6b78
unknown <unknown>
2021-12-15 16:15:06 +0100

    test

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
         }
       
         echo "<div class=\"form-group row\">\n";
-        echo "<label class=\"col-form-label\">".$nomCourse."</label>";
+        echo "<label class=\"col-sm-1 col-form-label\">".$nomCourse."</label>";
         echo "<div class=\"\">";
         echo "<input class=\"form-control\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
         echo "</div>\n";
