a3d380d8e1638295088f649bc24908a0e720337d
unknown <unknown>
2021-12-15 08:48:36 +0100

    test

--- charge.sh
+++ charge.sh
@@ -4,3 +4,10 @@
 git add *
 git commit -m "test"
 git push origin courses 
+
+for variable in 'courses.php' 'index.js' 'style.css'
+do
+
+  rcp $variable devphp1@192.168.0.1:/var/www/html/courses/
+
+done
