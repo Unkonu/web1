7a5f4b0eed7d270674236301960a72714897fac7
unknown <unknown>
2021-12-15 16:40:20 +0100

    test

--- index.php
+++ index.php
@@ -174,10 +174,10 @@
           // Affichage différent de la ligne
         }
       
-        echo "<div class=\"list-group row\">\n";
-        echo "<label class=\"list-group-item col-6 \">".$nomCourse."</label>";
+        echo "<div class=\"row\">\n";
+        echo "<label class=\"form-group-item col-6 \">".$nomCourse."</label>";
         echo "<div class=\"col-1\">";
-        echo "<input class=\"list-group-item form-control\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
+        echo "<input class=\"form-group-item form-control\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
         echo "</div>\n";
         echo "</div>\n";
 
