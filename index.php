ee2fbbaacad748b771a6a1ee22dc30b48552fa34
unknown <unknown>
2021-12-15 15:36:18 +0100

    test

--- index.php
+++ index.php
@@ -178,7 +178,7 @@
         echo "<div class=\"input-group mb-3\">\n";
         echo "<div class=\"input-group-prepend\">\n";
         echo $nomCourse;
-        echo "<input class=\"\" type=\"text\" value=\"".$nomCourse."\">\n";
+        echo "<input class=\"form-control\" type=\"text\" value=\"".$nomCourse."\">\n";
         echo "</div>\n";
         echo "<input class=\"form-control\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
         echo "</div>\n";
