8dc9635333e27d3327808c3409274f319f24f240
unknown <unknown>
2021-12-15 19:36:31 +0100

    test

--- index.php
+++ index.php
@@ -179,8 +179,8 @@
           // Affichage différent de la ligne
         }
       
-        echo "<div class=\"input-group mb-1 row\">\n";
-        echo "<a onclick=\"initElement(this);\" class=\"form-control ml-1 dark\">".$nomCourse."</a>";
+        echo "<div class=\"input-group mb-1 row swatch-blue\">\n";
+        echo "<a onclick=\"initElement(this);\" class=\"form-control ml-1\">".$nomCourse."</a>";
         echo "<div class=\"input-group-append col-2\">";
         echo "<input class=\"btn col-1\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
         echo "<input class=\"btn ml-1 col-1\" name=\"ligne_$ligneId\" type=\"submit\" value=\"ok\">\n"; 
