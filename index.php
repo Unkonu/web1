0a0373086530c3525517adebc0b58d80fc27dc57
unknown <unknown>
2021-12-15 18:37:35 +0100

    test

--- index.php
+++ index.php
@@ -180,7 +180,7 @@
         }
       
         echo "<div class=\"input-group mb-1 row\">\n";
-        echo "<a class=\"form-control ml-1\">".$nomCourse."</a>";
+        echo "<a onclick=\"this.disabled=true\" class=\"form-control ml-1\">".$nomCourse."</a>";
         echo "<div class=\"input-group-append col-2\">";
         echo "<input class=\"btn col-1\" name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">\n"; 
         echo "<input class=\"btn ml-1 col-1\" name=\"ligne_$ligneId\" type=\"submit\" value=\"ok\">\n"; 
