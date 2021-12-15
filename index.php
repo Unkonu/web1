10b96ff1dc409c48227d7958c29ca158e08b96c6
unknown <unknown>
2021-12-15 10:55:46 +0100

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <title>Traitement des donn&eacute;es du formulaire</title>
+  <title>Liste des courses</title>
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="style.css" />
   
@@ -13,89 +13,188 @@
 </head>
 <body>
   <header>
-    <a href="index.html">Retour formulaire</a>
-    <a href="liste.php">Afficher la liste des utilisateurs</a>
+    <?php
+      include 'courses_entete.php';
+    ?>  
   </header>
+
+  <?php
+  // On teste si une nouvelle course a été ajouté
+  
+   if (!empty($_POST)) {
+   
+      // echo "Traitement du formulaire method POST <br />";
+    
+      $action = $_POST['action'];
+
+      switch($action) {
+        case "ajout" :
+
+          $nom = $_POST['nomCourse'];
+          
+          if (!empty($nom)) {
+          
+          try {      
+            //On établit la connexion            
+            $conn = new PDO("mysql:host=".NOM_SERVEUR.";dbname=".NOM_BASE, UTILISATEUR, MOTDEPASSE);
+          
+            // echo "Connexion OK <br/>";
+                  
+            $requete = "INSERT INTO courses (nom,statut) 
+                        VALUES ('$nom',false)";
+
+            // echo "Resultats de la requete $requete <br />";
+            
+            if ($conn->exec($requete) === true) {
+            
+              // echo "Insertion réussie<br/>";
+
+            }// if
+            
+            // Fermeture connexion
+            $conn = null;
+            
+          } catch(PDOException $e) {
+            echo "Erreur : ", $e->getMessage(),"<br />";    
+          }// catch
+          
+          }// Fin if
+        
+        break; // case Ajout
+        
+        case "modif" :
+        
+          $nom = $_POST['nomCourse'];
+          
+          // parcours du tableau pour récupérer le numéro d'Id
+          foreach($_POST as $cle => $valeur) {
+          
+            // echo $cle."-".$valeur."<BR>\n";
+                        
+            if(!empty(strstr($cle,"ligne_"))) {
+            
+              $elements = explode("_", $cle);
+              $idCourse = $elements[1];
+                            
+              // echo "ID trouvé : $idCourse<BR>\n";
+            }// Fin if
+            
+          }// Fin foreach
+          
+          if (isset($idCourse)) {
+          
+            try {      
+              //On établit la connexion
+              $conn = new PDO("mysql:host=".NOM_SERVEUR.";dbname=".NOM_BASE, UTILISATEUR, MOTDEPASSE);
+            
+              // echo "Connexion OK <br/>";
+                    
+              $requete = "DELETE FROM courses WHERE id=$idCourse";
+
+              // echo "Resultats de la requete $requete <br />";
+              
+              if ($conn->exec($requete) === true) {
+              
+                echo "Suppression réussie<br/>";
+
+              }// if
+              
+              // Fermeture connexion
+              $conn = null;
+              
+            } catch(PDOException $e) {
+              echo "Erreur : ", $e->getMessage(),"<br />";    
+            }// catch
+      
+          }// Fin if
+          
+        break; // case Modif
+        
+        default: ;
+      
+      }// fin switch
+       
+    } else {
+   
+      // echo("<p>Le Formulaire n'a pas &eacute;t&eacute; rempli");
+
+    }// Fin if
+      
+  ?>  
 
   <section>
   
-  <?php
-  
-    $formulaire_valide = false;
- 
-    if (!empty($_GET)) {
-  
-      echo "Traitement du formulaire method GET <br />";
-      
-      $nom = $_GET['form1Nom'];
-      $prenom = $_GET['form1Prenom'];
-      $mail = $_GET['form1Mail'];
-      $tel = $_GET['form1Tel'];
-      $pass = $_GET['form1Pass'];
-      $role = $_GET['form1Role']; 
-    
-      $formulaire_valide = true;
-      
-    } else if (!empty($_POST)) {
-        echo "Traitement du formulaire method POST <br />";
-    
-      $nom = $_POST['form1Nom'];
-      $prenom = $_POST['form1Prenom'];
-      $mail = $_POST['form1Mail'];
-      $tel = $_POST['form1Tel'];
-      $pass = $_POST['form1Pass'];
-      $role = $_POST['form1Role'];
-       
-      $formulaire_valide = true;
- 
-    }
-    
-    if ($formulaire_valide == true) {
+  <div class="courses">
+  <form id="courses" enctype="multipart/form-data" method="post" action="courses.php">
+    <input id="action" name="action" type="hidden" value="ajout">
+    <fieldset>
+    <legend>Courses</legend>
+      <label for="nomCourse"></label>
+      <input id="nomCourse" name="nomCourse" type="text" value="" autofocus/>
 
-      $servername = 'localhost';
-      $username = 'devsql1';
-      $password = 'devsql1';
-      
-      try {      
-        //On établit la connexion
-        $conn = new PDO("mysql:host=$servername;dbname=base_test", $username, $password);
-      
-        echo "Connexion OK <br/>";
-              
-        $requete = "INSERT INTO utilisateurs (nom,prenom,mail,tel,password,role) 
-                    VALUES ('$nom','$prenom','$mail','$tel','$pass','$role')";
-
-        echo "Resultats de la requete $requete <br />";
-        
-        if ($conn->exec($requete) === true) {
-        
-          echo "Insertion réussie<br/>";
-
-        }// if
-        
-        // Fermeture connexion
-        $conn = null;
-        
-      } catch(PDOException $e) {
-        echo "Erreur : ", $e->getMessage(),"<br />";    
-      }// catch
-        
-    } else {
-    
-      echo("<p>Le Formulaire n'a pas &eacute;t&eacute; rempli");
-      
-    }
-      
-  ?>
+      <input id="ajouteCourse" type="submit" value="+" />
+    </fieldset>
+  </form>
   
-  
-  
+  </div>
 
   </section>
   
+  <section>
+  
+  <?php
+      
+    try {      
+      //On établit la connexion
+      $conn = new PDO("mysql:host=".NOM_SERVEUR.";dbname=".NOM_BASE, UTILISATEUR, MOTDEPASSE);
+    
+      // echo "Connexion OK <br/>";
+            
+      $requete = "SELECT * FROM courses ORDER BY id";
+
+      // echo "Resultats de la requete $requete <br />";
+      
+      // Construction de la liste
+      echo "<div class=\"listeCourses\"><ul>\n";
+      echo "<form id=\"liste\" class=\"formModif\" enctype=\"multipart/form-data\" method=\"post\" action=\"courses.php\">\n";
+      echo "<input id=\"action\" name=\"action\" type=\"hidden\" value=\"modif\">\n";
+
+      foreach ($conn->query($requete) as $ligne) {
+      
+        $ligneId = $ligne['id'];
+        $nomCourse = $ligne['nom'];
+        $statut = $ligne['statut'];
+      
+        if ($statut) {
+          // Affichage différent de la ligne
+        }
+      
+        echo "<li>";
+        echo $nomCourse;
+        echo "<input name=\"ligne_$ligneId\" type=\"submit\" value=\"-\">"; 
+        echo "</li>\n";
+
+      }// foreach
+
+      echo "</form>\n";
+      echo "</ul></div>\n";
+      
+      // Fermeture connexion
+      $conn = null;
+      
+    } catch(PDOException $e) {
+      echo "Erreur : ", $e->getMessage(),"<br />\n";    
+    }// catch
+    
+      
+  ?>
+  
+  </section>
+  
   <footer>
-  
-  
+    <?php
+      include 'courses_pied.php';
+    ?>  
   </footer>
   
 </body>
