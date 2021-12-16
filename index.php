375e0b173f1ca5e60c3018253277e32db0476bc6
unknown <unknown>
2021-12-16 10:06:38 +0100

    test

--- index.php
+++ index.php
@@ -163,7 +163,7 @@
   <?php
 
     // Construction de la liste
-    echo "<div width=400 class=\"container row-6 overflow-auto\">\n";
+    echo "<div class=\"row-6 overflow-auto\">\n";
     echo "<form name=\"formModif\" class=\"\" enctype=\"multipart/form-data\" method=\"post\" action=\"index.php\">\n";
     echo "<input id=\"action\" name=\"action\" type=\"hidden\" value=\"modif\">\n";
       
